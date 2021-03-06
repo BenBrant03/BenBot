764eedde4797b92488de5f616056e83810c4fc76
Ben Brant <ben.brant23@gmail.com>
2021-03-06 18:16:39 +0000

    Added score preditor

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'test',
     description: "Used to Test new features",
     execute(message, args){
-        message.channel.send('Hi This is Ben Bot V 1.05')
+        message.channel.send('Hi This is Ben Bot V 1.06')
 
     }
 }
